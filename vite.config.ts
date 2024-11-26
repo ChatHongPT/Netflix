40997d9179c7c7d57016bf70e72157d6fadea5fc
suk924600 <suk924600@naver.com>
2024-11-26 18:15:53 +0900

    error

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-<<<<<<< HEAD
   base: '/Netflix/',
 })
-=======
-})
->>>>>>> develop

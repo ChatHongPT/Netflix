f8ddaa0faa381b6015c5b9c523fd9cff09a1ae6a
suk924600 <suk924600@naver.com>
2024-11-27 00:16:26 +0900

    내가 찜한 콘텐츠 생성성

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import VueI18n from '@intlify/unplugin-vue-i18n/vite';
 
 export default defineConfig({
-  plugins: [vue()],
-  base: '/Netflix/',
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag === 'v-intersect'
+        }
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': '/src',

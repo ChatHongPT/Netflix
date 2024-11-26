eecd87ffc8c7088f1d67f31dda61f67fff552ebf
suk924600 <suk924600@naver.com>
2024-11-26 18:08:50 +0900

    error edit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Netflix/', // 저장소 이름과 일치시켜야 합니다.
-});
+})
